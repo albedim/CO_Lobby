c7e3a284876ae916d238e6ba9d96216a101ca1d2
albedim <dimaio.albe@gmail.com>
2023-09-08 15:41:11 +0200

    [feature] - bcs added

--- src/main/java/it/craftopoly/co_lobby/CO_Lobby.java
+++ src/main/java/it/craftopoly/co_lobby/CO_Lobby.java
@@ -3,6 +3,10 @@ package it.craftopoly.co_lobby;
 import it.craftopoly.co_lobby.listener.*;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public final class CO_Lobby extends JavaPlugin {
 
@@ -19,6 +23,7 @@ public final class CO_Lobby extends JavaPlugin {
         Bukkit.getPluginManager().registerEvents(new ItemDropEvent(), this);
         Bukkit.getPluginManager().registerEvents(new JoinEvent(), this);
         Bukkit.getPluginManager().registerEvents(new ConsumeEvent(), this);
+        sendBroadcastMessages();
     }
 
     @Override
@@ -29,5 +34,33 @@ public final class CO_Lobby extends JavaPlugin {
     public static CO_Lobby getInstance()
     {
         return plugin;
+    }
+
+    private void sendBroadcastMessages()
+    {
+        List<String> broadcastMessages = CO_Lobby.getInstance().getConfig().getStringList("bcs");
+        List<Integer> sentBroadcastMessages = new ArrayList<>();
+
+        new BukkitRunnable()
+        {
+            @Override
+            public void run()
+            {
+                int counter = (int) Math.floor(Math.random() * broadcastMessages.size());
+
+                while(!sentBroadcastMessages.contains(counter) &&
+                        sentBroadcastMessages.size() != broadcastMessages.size()) {
+                    Bukkit.broadcastMessage(broadcastMessages.get(counter));
+                    sentBroadcastMessages.add(counter);
+                }
+
+                if(sentBroadcastMessages.size() == broadcastMessages.size())
+                    sentBroadcastMessages.clear();
+            }
+        }.runTaskTimerAsynchronously(
+                this,
+                Long.parseLong(this.getConfig().getString("bcs_time")),
+                Long.parseLong(this.getConfig().getString("bcs_time"))
+        );
     }
 }
